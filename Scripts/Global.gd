361d0c8c44599faeae7ae8fd6b5e33493a1c1fca
Valeriy <hispam@ukr.net>
2021-07-23 18:39:16 +0300

    Reorginize project tree

---
+++ Scripts/Global.gd
@@ -0,0 +1,16 @@
+extends Node
+
+enum Layers {
+	FIREABLE = 0b1,
+	UNFIREABLE = 0b10,
+	TREE = 0b100,
+	ITEM = 0b1000,
+	STONE = 0b10000
+}
+
+func _ready():
+	pass
+
+# Get main 
+func get_main():
+	return get_node("/root/Main")
---
+++ Scripts/Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_New_pressed():
+	get_tree().change_scene("res://Scenas/Main.tscn")
+
+
+func _on_Score_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
