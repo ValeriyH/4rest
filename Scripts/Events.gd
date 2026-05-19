4b1e9f037ef7f51d12b1226d18873f8b0f2b34d6
valeriy <valeriy>
2026-05-19 20:57:45 +0300

    Update event system

--- Scripts/Events.gd
+++ Scripts/Events.gd
@@ -1,7 +1,12 @@
 extends Node
 
+#This is a Node with Global Events
+
 const TREE_PLANT = "tree_plant"
 signal tree_plant(tree_instance)
 
 const TREE_BURN = "tree_burn"
 signal tree_burn(tree_instance)
+
+const ITEM_SELECT = "item_select"
+signal item_select(item)
